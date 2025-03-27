ea75f8c28276f93844260c4dfe052d15c732d781
Yukai Huang <yukaihuangtw@gmail.com>
2025-03-27 19:33:09 +0800

    feat: create development map page with festival map overlay component

---
+++ app/dev/map/page.tsx
@@ -0,0 +1,10 @@
+import FestivalMapOverlay from "@/components/festival-map-overlay";
+
+export default function DevMapPage() {
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-2xl font-bold mb-4">Development Map Page</h1>
+      <FestivalMapOverlay />
+    </div>
+  );
+}
